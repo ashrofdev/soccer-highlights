347509e4ee35548931c2286411f28de6a07b9f10
First Last <salmanashraf853@gmail.com>
2020-02-18 23:20:57 +0100

    fetching the videos API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import './app.css'
 
 
 class App extends Component {
+
+
+  componentDidMount(){
+
+    // this is the api fetch
+    
+    fetch("https://free-football-soccer-videos.p.rapidapi.com/", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "free-football-soccer-videos.p.rapidapi.com",
+        "x-rapidapi-key": "34857597femsh6ed6990c729453fp1fdddcjsn7a5471db02b3"
+      }
+    })
+    .then(response => {
+      console.log(response.json());
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
  
   render() {
     
